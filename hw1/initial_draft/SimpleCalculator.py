10639386b25e4992358f7ffd773a1e41c8241b5a
jugu <jugu.87@gmail.com>
2016-02-08 00:08:28 -0500

    Simple Calculator

--- hw1/initial_draft/SimpleCalculator.py
+++ hw1/initial_draft/SimpleCalculator.py
@@ -1,5 +1,6 @@
 '''Simple Calculator'''
-
+import re
+from exception import CustomException
 class SimpleCalculator(object):
     def getUserInput(self):
         instText = ''
@@ -9,3 +10,28 @@ class SimpleCalculator(object):
                 break
             instText += line + '\n'        
         return instText
+    
+    '''
+        x = 10;
+        z = ~10;
+        y = - x 1;
+        z = * x * y + x y;
+        
+        1. Make sure all characters are valid in input
+        2. Split text based on ';'
+        3. Split text based on '='
+        4. LHS should a valid variable construction( alphanumeric, only one word...)
+        5. RHS should be a valid expression construction
+        6. Maintain map for initialized variables. If uninialized variable found then throw exception
+        
+        
+        
+    '''
+        
+if __name__ == "__main__":
+    sc = SimpleCalculator()
+    iText = sc.getUserInput()
+    
+    
+        
+        
