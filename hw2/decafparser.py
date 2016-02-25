27ec162092ff84763a854341d2c593ebcd364a18
jugu <jugu.87@gmail.com>
2016-02-25 15:10:43 -0500

    parser

--- hw2/decafparser.py
+++ hw2/decafparser.py
@@ -13,47 +13,30 @@ precedence = (
     ('left', 'LT', 'GT', 'LEQ', 'GEQ'),
     ('left', 'PLUS'),
     ('left', 'MULT', 'DIV'),
-    ('left', 'NOT')
+    ('left', 'NOT'),
 )
 
-def p_expression_plus(p):
-    'expression : expression PLUS term'
-    p[0] = p[1] + p[3]
+start = 'program'
 
-def p_expression_minus(p):
-    'expression : expression MINUS term'
-    p[0] = p[1] - p[3]
+def p_program(p):
+    'classdecl*'
 
-def p_expression_term(p):
-    'expression : term'
-    p[0] = p[1]
-
-def p_term_times(p):
-    'term : term TIMES factor'
-    p[0] = p[1] * p[3]
-
-def p_term_div(p):
-    'term : term DIVIDE factor'
-    p[0] = p[1] / p[3]
-
-def p_term_factor(p):
-    'term : factor'
-    p[0] = p[1]
-
-def p_factor_num(p):
-    'factor : NUMBER'
-    p[0] = p[1]
-
+def p_classdecl(p):
+    '''classdecl : CLASS IDENTIFIER LPAREN PLUS RPAREN
+                  | CLASS IDENTIFIER EXTENDS IDENTIFIER LPAREN PLUS RPAREN'''
+             
+#def p_class_body_decl(p):
+ #   '''class_body_decl : field_decl
+  #                      | method_decl
+   #                     | constructor_decl'''
+    
+#def p_
+    
 ROOT_FOLDER = 'F:\\MastersStonyBrook\\SemesterCourses\\Semester2\\CSE504_Compilers\\jsundar-sahjain\\hw2\\'
 
 if __name__ == '__main__':
     #file=open(ROOT_FOLDER+'test_case_1.txt')
     #data = file.read()
-    data = 'x= 3+5;'
-    lexer = lex.lex()
-    lexer.input(data)
+    data = 'class temp { + }'
     parser = yacc.yacc()
     p = parser.parse(data)
-    if parser.error: 
-        print None
-    print p
