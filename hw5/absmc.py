f2a3aeb5cbd32623dec80f0159a9fd17f8ab7f04
Sahil Jain <sahil.2441@gmail.com>
2016-04-21 10:02:58 -0400

    Adding Functions for instructions.

--- hw5/absmc.py
+++ hw5/absmc.py
@@ -8,6 +8,35 @@ import ast
 class Instruction:
 	pass
 
+class Move_Immed_i_Instruction(Instruction):
+	def __init__(self, ra, rb):
+		self.ra = ra
+		self.rb = rb
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.MOVE_IMMED_I, self.ra, self.rb])
+
+class Move_Immed_f_Instruction(Instruction):
+	def __init__(self, ra, rb):
+		self.ra = ra
+		self.rb = rb
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.MOVE_IMMED_F, self.ra, self.rb])
+
+class Move_Instruction(Instruction):
+	def __init__(self, ra, rb):
+		self.ra = ra
+		self.rb = rb
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.MOVE, self.ra, self.rb])
+
+######################################################################
+######################################################################
+
+# Definition for integer
+
 class AddInstruction(Instruction):
 	def __init__(self, ra, rb, rc):
 		self.ra = ra
@@ -17,6 +46,303 @@ class AddInstruction(Instruction):
 	def __str__(self):
 		return " ".join([INSTRUCTION.IADD, self.ra, self.rb , self.rc])
 
+class SubInstruction(Instruction):
+	def __init__(self, ra, rb, rc):
+		self.ra = ra
+		self.rb = rb
+		self.rc = rc
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.ISUB, self.ra, self.rb , self.rc])
+
+class MulInstruction(Instruction):
+	def __init__(self, ra, rb, rc):
+		self.ra = ra
+		self.rb = rb
+		self.rc = rc
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.IMUL, self.ra, self.rb , self.rc])
+
+class DivInstruction(Instruction):
+	def __init__(self, ra, rb, rc):
+		self.ra = ra
+		self.rb = rb
+		self.rc = rc
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.IDIV, self.ra, self.rb , self.rc])
+
+class ModInstruction(Instruction):
+	def __init__(self, ra, rb, rc):
+		self.ra = ra
+		self.rb = rb
+		self.rc = rc
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.IMOD, self.ra, self.rb , self.rc])
+
+class GtInstruction(Instruction):
+	def __init__(self, ra, rb, rc):
+		self.ra = ra
+		self.rb = rb
+		self.rc = rc
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.IGT, self.ra, self.rb , self.rc])
+
+class GeqInstruction(Instruction):
+	def __init__(self, ra, rb, rc):
+		self.ra = ra
+		self.rb = rb
+		self.rc = rc
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.IGEQ, self.ra, self.rb , self.rc])
+
+class LtInstruction(Instruction):
+	def __init__(self, ra, rb, rc):
+		self.ra = ra
+		self.rb = rb
+		self.rc = rc
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.ILT, self.ra, self.rb , self.rc])
+
+class LeqInstruction(Instruction):
+	def __init__(self, ra, rb, rc):
+		self.ra = ra
+		self.rb = rb
+		self.rc = rc
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.ILEQ, self.ra, self.rb , self.rc])
+
+class LtInstruction(Instruction):
+	def __init__(self, ra, rb, rc):
+		self.ra = ra
+		self.rb = rb
+		self.rc = rc
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.ILT, self.ra, self.rb , self.rc])
+
+######################################################################
+######################################################################
+
+# similar definitions for float
+
+class AddInstructionFloat(Instruction):
+	def __init__(self, ra, rb, rc):
+		self.ra = ra
+		self.rb = rb
+		self.rc = rc
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.FADD, self.ra, self.rb , self.rc])
+
+class SubInstructionFloat(Instruction):
+	def __init__(self, ra, rb, rc):
+		self.ra = ra
+		self.rb = rb
+		self.rc = rc
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.FSUB, self.ra, self.rb , self.rc])
+
+class MulInstructionFloat(Instruction):
+	def __init__(self, ra, rb, rc):
+		self.ra = ra
+		self.rb = rb
+		self.rc = rc
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.FMUL, self.ra, self.rb , self.rc])
+
+class DivInstructionFloat(Instruction):
+	def __init__(self, ra, rb, rc):
+		self.ra = ra
+		self.rb = rb
+		self.rc = rc
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.FDIV, self.ra, self.rb , self.rc])
+
+class ModInstructionFloat(Instruction):
+	def __init__(self, ra, rb, rc):
+		self.ra = ra
+		self.rb = rb
+		self.rc = rc
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.FMOD, self.ra, self.rb , self.rc])
+
+class GtInstructionFloat(Instruction):
+	def __init__(self, ra, rb, rc):
+		self.ra = ra
+		self.rb = rb
+		self.rc = rc
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.FGT, self.ra, self.rb , self.rc])
+
+class GeqInstructionFloat(Instruction):
+	def __init__(self, ra, rb, rc):
+		self.ra = ra
+		self.rb = rb
+		self.rc = rc
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.FGEQ, self.ra, self.rb , self.rc])
+
+class LtInstructionFloat(Instruction):
+	def __init__(self, ra, rb, rc):
+		self.ra = ra
+		self.rb = rb
+		self.rc = rc
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.FLT, self.ra, self.rb , self.rc])
+
+class LeqInstructionFloat(Instruction):
+	def __init__(self, ra, rb, rc):
+		self.ra = ra
+		self.rb = rb
+		self.rc = rc
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.FLEQ, self.ra, self.rb , self.rc])
+
+class LtInstructionFloat(Instruction):
+	def __init__(self, ra, rb, rc):
+		self.ra = ra
+		self.rb = rb
+		self.rc = rc
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.FLT, self.ra, self.rb , self.rc])
+
+######################################################################
+######################################################################
+
+class Itof_Instruction(Instruction):
+	def __init__(self, ra, rb):
+		self.ra = ra
+		self.rb = rb
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.ITOF, self.ra, self.rb])
+
+class Itoi_Instruction(Instruction):
+	def __init__(self, ra, rb):
+		self.ra = ra
+		self.rb = rb
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.FTOI, self.ra, self.rb])
+
+######################################################################
+######################################################################
+
+class HloadInstruction(Instruction):
+	def __init__(self, ra, rb, rc):
+		self.ra = ra
+		self.rb = rb
+		self.rc = rc
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.HLOAD, self.ra, self.rb , self.rc])
+
+class HstoreInstruction(Instruction):
+	def __init__(self, ra, rb, rc):
+		self.ra = ra
+		self.rb = rb
+		self.rc = rc
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.HSTORE, self.ra, self.rb , self.rc])
+
+class HallocInstruction(Instruction):
+	def __init__(self, ra, rb, rc):
+		self.ra = ra
+		self.rb = rb
+		self.rc = rc
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.HALLOC, self.ra, self.rb , self.rc])
+
+######################################################################
+######################################################################
+
+class Bz_Instruction(Instruction):
+	def __init__(self, ra, rb):
+		self.ra = ra
+		self.rb = rb
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.BZ, self.ra, self.rb])
+
+class Bnz_Instruction(Instruction):
+	def __init__(self, ra, rb):
+		self.ra = ra
+		self.rb = rb
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.BNZ, self.ra, self.rb])
+
+class Jmp_Instruction(Instruction):
+	def __init__(self, ra):
+		self.ra = ra
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.JMP, self.ra])
+
+######################################################################
+######################################################################
+
+class Call_Instruction(Instruction):
+	def __init__(self, ra):
+		self.ra = ra
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.CALL, self.ra])
+
+class Ret_Instruction(Instruction):
+	def __init__(self): pass
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.RET])
+
+class Save_Instruction(Instruction):
+	def __init__(self, ra):
+		self.ra = ra
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.SAVE, self.ra])
+
+class Restore_Instruction(Instruction):
+	def __init__(self, ra):
+		self.ra = ra
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.RESTORE, self.ra])
+
+class Peek_Instruction(Instruction):
+	def __init__(self, ra):
+		self.ra = ra
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.PEEK, self.ra])
+
+class Nop_Instruction(Instruction):
+	def __init__(self, ra):
+		self.ra = ra
+
+	def __str__(self):
+		return " ".join([INSTRUCTION.NOP, self.ra])
+
+######################################################################
+######################################################################
 
 class INSTRUCTION(object):
     instList =  [ 'move_immed_i',
